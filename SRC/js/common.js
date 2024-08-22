91064d7b70a53f3259f4920020e3c9b99c03ac77
hanayamaharuka <202406ap01_hanayama@x-ide.sakura.ne.jp>
2024-08-22 15:07:26 +0900

    ファイル名：仕入管理
    シート名：仕入管理
    バグNo：214
    
    空白のまま登録してもエラーメッセージが表示される件の解消

--- SRC/js/common.js
+++ SRC/js/common.js
@@ -5,20 +5,21 @@
  * @return true:正常、false:異常
  */
 function fnYMDCheck(msg, obj) {
-	// 未入力時はチェックしない
-	oYMD = obj.value;
-
-	var tmp = oYMD.split('/');
-	var ymd = new Date(tmp[0], parseInt(tmp[1], 10) - 1, parseInt(tmp[2], 10));
-	var vYMD = ymd.getFullYear() + '/' + ('0' + (ymd.getMonth() + 1)).slice(-2) + '/' + ('0' + ymd.getDate()).slice(-2);
-	if (oYMD == vYMD) {
-		return true;
-	} else {
-		alert(msg + "を入力してください");
-		return false;
-	}
+  // 未入力時はチェックしない
+  oYMD = obj.value;
+  if (!oYMD) {
+    return true;
+  }
+  var tmp = oYMD.split('/');
+  var ymd = new Date(tmp[0], parseInt(tmp[1], 10) - 1, parseInt(tmp[2], 10));
+  var vYMD = ymd.getFullYear() + '/' + ('0' + (ymd.getMonth() + 1)).slice(-2) + '/' + ('0' + ymd.getDate()).slice(-2);
+  if (oYMD == vYMD) {
+    return true;
+  } else {
+    alert(msg + "を入力してください");
+    return false;
+  }
 }
-
 
 
 /**
@@ -30,11 +31,11 @@ function fnYMDCheck(msg, obj) {
  * @return true:異常、false:正常
  */
 function isLength(length, msg, obj) {
-	rtn = false;
-	if (obj.value.length > length) {
-		alert(msg + "は" + length + "文字以内で入力して下さい");
-	}
-	return rtn;
+  rtn = false;
+  if (obj.value.length > length) {
+    alert(msg + "は" + length + "文字以内で入力して下さい");
+  }
+  return rtn;
 }
 
 
@@ -48,10 +49,10 @@ function isLength(length, msg, obj) {
  * @return true:異常、false:正常
  */
 function isNumericLength(length, msg, obj) {
-	rtn = false;
-	if (obj.value.length > 9 || obj.value.match(/[^0-8]+/)) {
-		alert(msg + "は" + length + "桁以内の半角数字で入力してください");
-		rtn = true;
-	}
-	return rtn;
+  rtn = false;
+  if (obj.value.length > 9 || obj.value.match(/[^0-8]+/)) {
+    alert(msg + "は" + length + "桁以内の半角数字で入力してください");
+    rtn = true;
+  }
+  return rtn;
 }
