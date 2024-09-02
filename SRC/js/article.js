46da3f6e9704e0b9b027436d4fbe29f87cf41a88
hanayamaharuka <202406ap01_hanayama@x-ide.sakura.ne.jp>
2024-09-02 17:14:38 +0900

    ファイル名：業者管理
    シート名：業者管理
    バグNo：52
    
    物件名の入力をしてもエラーが表示される件の解消

--- SRC/js/article.js
+++ SRC/js/article.js
@@ -2,23 +2,23 @@
 //物件管理チェック
 //
 function fnArticleEditCheck() {
-	tmp = form.article.value;
-	if (tmp) {
-		alert('物件名を入力してください');
-		return;
-	}
-	if (isLength(100, "物件名", form.article)) { return; }
+  tmp = form.article.value;
+  if (tmp.length == 0) {
+    alert('物件名を入力してください');
+    return;
+  }
+  if (isLength(100, "物件名", form.article)) { return; }
 
-	form.act.value = 'articleEditComplete';
-	form.submit();
+  form.act.value = 'articleEditComplete';
+  form.submit();
 }
 
 
 
 function fnArticleDeleteCheck(no) {
-	if (confirm('削除します。よろしいですか？')) {
-		form.articleNo.value = no;
-		form.act.value = 'articleDelete';
-		form.submit();
-	}
+  if (confirm('削除します。よろしいですか？')) {
+    form.articleNo.value = no;
+    form.act.value = 'articleDelete';
+    form.submit();
+  }
 }
