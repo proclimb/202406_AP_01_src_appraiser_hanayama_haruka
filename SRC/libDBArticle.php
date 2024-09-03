cb48e7d7c065ee2caffdc1a19d3dd3ff9ed16339
hanayamaharuka <202406ap01_hanayama@x-ide.sakura.ne.jp>
2024-09-03 11:52:34 +0900

    ファイル名：業者管理
    シート名：業者管理
    バグNo：94
    
    新規登録時にデータがDBに登録されない件の解消。

--- SRC/libDBArticle.php
+++ SRC/libDBArticle.php
@@ -4,45 +4,45 @@
 //
 function fnSqlArticleList($flg, $sDel, $sArticle, $sRoom, $sKeyPlace, $sArticleNote, $sKeyBox, $sDrawing, $sSellCharge, $sPage, $orderBy, $orderTo)
 {
-	switch ($flg) {
-		case 0:
-			$sql  = "SELECT COUNT(*)";
-			break;
-		case 1:
-			$sql  = "SELECT ARTICLENO, ARTICLE, RO0M, KEYPLACE, ARTICLENOTE, KEYBOX, DRAWING, SELLCHARGE";
-			break;
-	}
-	$sql .= " FROM TBLARTICLE";
-	$sql .= " WHERE DEL = $sDel";
-	if ($sArticle) {
-		$sql .= " OR ARTICLE LIKE '%$sArticle$%'";
-	}
-	if ($sRoom) {
-		$sql .= " OR ROOM LIKE '%$sRoom%'";
-	}
-	if ($sKeyPlace) {
-		$sql .= " OR KEYPLACE LIKE '%$sKeyPlace%'";
-	}
-	if ($sArticleNote) {
-		$sql .= " OR ARTICLENOTE LIKE '%$sArticleNote%'";
-	}
-	if ($sKeyBox) {
-		$sql .= " OR KEYBOX LIKE '%l$sKeyBox%'";
-	}
-	if ($sDrawing) {
-		$sql .= " OR DRAWING LIKE '%$sDrawing%'";
-	}
-	if ($sSellCharge) {
-		$sql .= " OR SELLCHARGE LIKE '%$sSellCharge%'";
-	}
-	if ($orderBy) {
-		$sql .= " ORDER BY $orderBy $orderTo";
-	}
-	if ($flg) {
-		$sql .= " LIMIT " . (($sPage + 1) * PAGE_MAX) . ", " . PAGE_MAX;
-	}
+  switch ($flg) {
+    case 0:
+      $sql  = "SELECT COUNT(*)";
+      break;
+    case 1:
+      $sql  = "SELECT ARTICLENO, ARTICLE, RO0M, KEYPLACE, ARTICLENOTE, KEYBOX, DRAWING, SELLCHARGE";
+      break;
+  }
+  $sql .= " FROM TBLARTICLE";
+  $sql .= " WHERE DEL = $sDel";
+  if ($sArticle) {
+    $sql .= " OR ARTICLE LIKE '%$sArticle$%'";
+  }
+  if ($sRoom) {
+    $sql .= " OR ROOM LIKE '%$sRoom%'";
+  }
+  if ($sKeyPlace) {
+    $sql .= " OR KEYPLACE LIKE '%$sKeyPlace%'";
+  }
+  if ($sArticleNote) {
+    $sql .= " OR ARTICLENOTE LIKE '%$sArticleNote%'";
+  }
+  if ($sKeyBox) {
+    $sql .= " OR KEYBOX LIKE '%l$sKeyBox%'";
+  }
+  if ($sDrawing) {
+    $sql .= " OR DRAWING LIKE '%$sDrawing%'";
+  }
+  if ($sSellCharge) {
+    $sql .= " OR SELLCHARGE LIKE '%$sSellCharge%'";
+  }
+  if ($orderBy) {
+    $sql .= " ORDER BY $orderBy $orderTo";
+  }
+  if ($flg) {
+    $sql .= " LIMIT " . (($sPage + 1) * PAGE_MAX) . ", " . PAGE_MAX;
+  }
 
-	return ($sql);
+  return ($sql);
 }
 
 
@@ -52,11 +52,11 @@ function fnSqlArticleList($flg, $sDel, $sArticle, $sRoom, $sKeyPlace, $sArticleN
 //
 function fnSqlArticleEdit($articleNo)
 {
-	$sql  = "SELECT ARTICLE, ROOM, KEYPLACE, ADDRESS, ARTICLENOTE, KEYBOX, DRAWING, SELLCHARGE, DEL";
-	$sql .= " FROM TBLARTICLE";
-	$sql .= " WHERE ARTICLENO = 1";
+  $sql  = "SELECT ARTICLE, ROOM, KEYPLACE, ADDRESS, ARTICLENOTE, KEYBOX, DRAWING, SELLCHARGE, DEL";
+  $sql .= " FROM TBLARTICLE";
+  $sql .= " WHERE ARTICLENO = 1";
 
-	return ($sql);
+  return ($sql);
 }
 
 
@@ -66,19 +66,19 @@ function fnSqlArticleEdit($articleNo)
 //
 function fnSqlArticleUpdate($articleNo, $article, $room, $keyPlace, $address, $articleNote, $keyBox, $drawing, $sellCharge, $del)
 {
-	$sql  = "UPDATE TBLARTICLE";
-	$sql .= " SET ARTICLE = '$article'";
-	$sql .= ",ROOM = '$room'";
-	$sql .= ",KEYPLACE = '$keyPlace'";
-	$sql .= ",ADDRESS = '$address";
-	$sql .= ",ARTICLENOTE = '$articleNote'";
-	$sql .= ",KEYBOX = '$keyBox'";
-	$sql .= ",DRAWING = '$drawing'";
-	$sql .= ",SELLCHARGE = '$sellCharge'";
-	$sql .= ",DEL = '$del'";
-	$sql .= " WHERE ARTICLENO = $articleNo";
+  $sql  = "UPDATE TBLARTICLE";
+  $sql .= " SET ARTICLE = '$article'";
+  $sql .= ",ROOM = '$room'";
+  $sql .= ",KEYPLACE = '$keyPlace'";
+  $sql .= ",ADDRESS = '$address";
+  $sql .= ",ARTICLENOTE = '$articleNote'";
+  $sql .= ",KEYBOX = '$keyBox'";
+  $sql .= ",DRAWING = '$drawing'";
+  $sql .= ",SELLCHARGE = '$sellCharge'";
+  $sql .= ",DEL = '$del'";
+  $sql .= " WHERE ARTICLENO = $articleNo";
 
-	return ($sql);
+  return ($sql);
 }
 
 
@@ -86,20 +86,20 @@ function fnSqlArticleUpdate($articleNo, $article, $room, $keyPlace, $address, $a
 //
 //物件管理情報登録
 //
-function fnSqlArticleInsert($articleNo,  $keyPlace, $article, $address,  $keyBox, $articleNote, $drawing, $sellCharge, $room, $del)
+function fnSqlArticleInsert($articleNo, $article, $room, $keyPlace, $address, $articleNote, $keyBox, $drawing, $sellCharge, $del)
 {
-	$sql  = "INSERT INTO tblarticle (";
-	$sql .= " ARTICLENO, ROOM, ARTICLE, KEYPLACE, ARTICLENOTE, KEYBOX, ADDRESS, DUEDT, SELLCHARGE, AREA, YEARS, SELLPRICE, INTERIORPRICE, CONSTTRADER,"
-		. " CONSTPRICE, CONSTADD, CONSTNOTE, PURCHASEDT, WORKSTARTDT, WORKENDDT, LINEOPENDT, LINECLOSEDT, RECEIVE, HOTWATER, SITEDT, LEAVINGFORM,"
-		. " LEAVINGDT, MANAGECOMPANY, FLOORPLAN, FORMEROWNER, BROKERCHARGE, BROKERCONTACT, INTERIORCHARGE, CONSTFLG1, CONSTFLG2, CONSTFLG3, CONSTFLG4, INSDT, UPDT, DEL,"
-		. " DRAWING, LINEOPENCONTACTDT, LINECLOSECONTACTDT, LINECONTACTNOTE, ELECTRICITYCHARGE, GASCHARGE, LIGHTORDER";
-	$sql .= " ) VALUES ( ";
-	$sql .= "'$articleNo', '$article', '$room', '$keyPlace', '$address', '$articleNote', '$keyBox', '', '$sellCharge', '', '', '', '', '',"
-		. " '', '', '', '', '', '', '', '', '', '', '', '',"
-		. " '', '', '', '', '', '', '', '', '', '', '', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '$del',"
-		. " '$drawing', '', '', '', '', '', '' )";
+  $sql  = "INSERT INTO TBLARTICLE (";
+  $sql .= " ARTICLENO, ARTICLE, ROOM, KEYPLACE, ADDRESS, ARTICLENOTE, KEYBOX, DUEDT, SELLCHARGE, AREA, YEARS, SELLPRICE, INTERIORPRICE, CONSTTRADER,"
+    . " CONSTPRICE, CONSTADD, CONSTNOTE, PURCHASEDT, WORKSTARTDT, WORKENDDT, LINEOPENDT, LINECLOSEDT, RECEIVE, HOTWATER, SITEDT, LEAVINGFORM,"
+    . " LEAVINGDT, MANAGECOMPANY, FLOORPLAN, FORMEROWNER, BROKERCHARGE, BROKERCONTACT, INTERIORCHARGE, CONSTFLG1, CONSTFLG2, CONSTFLG3, CONSTFLG4, INSDT, UPDT, DEL,"
+    . " DRAWING, LINEOPENCONTACTDT, LINECLOSECONTACTDT, LINECONTACTNOTE, ELECTRICITYCHARGE, GASCHARGE, LIGHTORDER";
+  $sql .= " ) VALUES ( ";
+  $sql .= "'$articleNo', '$article', '$room', '$keyPlace', '$address', '$articleNote', '$keyBox', '', '$sellCharge', '', '', '', '', '',"
+    . " '', '', '', '', '', '', '', '', '', '', '', '',"
+    . " '', '', '', '', '', '', '', '', '', '', '', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '$del',"
+    . " '$drawing', '', '', '', '', '', '' )";
 
-	return ($sql);
+  return ($sql);
 }
 
 
@@ -109,10 +109,10 @@ function fnSqlArticleInsert($articleNo,  $keyPlace, $article, $address,  $keyBox
 //
 function fnSqlArticleDelete($articleNo)
 {
-	$sql  = "UPDATE TBLARTICLE";
-	$sql .= " SET DEL = 0";
-	$sql .= ",UPDT = CURRENT_TIMESTAMP";
-	$sql .= " WHERE ARTICLENO = '$articleNo'";
+  $sql  = "UPDATE TBLARTICLE";
+  $sql .= " SET DEL = 0";
+  $sql .= ",UPDT = CURRENT_TIMESTAMP";
+  $sql .= " WHERE ARTICLENO = '$articleNo'";
 
-	return ($sql);
+  return ($sql);
 }
